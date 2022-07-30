387f211cad09165a7092d02559b5e62420f41520
Jason Axelson <jason.axelson@gmail.com>
2022-07-30 10:53:46 -1000

    Run the actual query

--- lib/gh_stats.ex
+++ lib/gh_stats.ex
@@ -1,6 +1,12 @@
 defmodule GhStats do
-  # https://docs.github.com/en/graphql/guides/forming-calls-with-graphql#the-graphql-endpoint
   def demo do
+    Neuron.Config.set(url: "https://api.github.com/graphql")
+    query = File.read!(Path.join([__DIR__, "gh_stats/pull_request_query.graphql"]))
+    run_query(query)
+  end
+
+  # https://docs.github.com/en/graphql/guides/forming-calls-with-graphql#the-graphql-endpoint
+  def demo2 do
     Neuron.Config.set(url: "https://api.github.com/graphql")
 
     Neuron.query(
@@ -11,6 +17,14 @@ defmodule GhStats do
         }
       }
       """,
+      %{},
+      headers: [authorization: "Bearer #{gh_token()}"]
+    )
+  end
+
+  defp run_query(query) do
+    Neuron.query(
+      query,
       %{},
       headers: [authorization: "Bearer #{gh_token()}"]
     )
